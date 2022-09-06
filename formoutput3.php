2d89bfb020f3468b60b5f99b65dd306027efa85f
jennamacwe <107898237+jennamacwe@users.noreply.github.com>
2022-09-06 15:51:36 +0700

    Add files via upload

---
+++ formoutput3.php
@@ -0,0 +1,23 @@
+<table>
+    <form action="output3tampil.php" method="POST" enctype="multipart/form-data">
+        <tr>
+            <td>nama</td>
+            <td>:<input type="text" name="nama"></td>
+        </tr>
+        <tr>
+            <td>tanggal lahir</td>
+            <td>:<input type="date" name="tgl"></td>
+        </tr>
+        <tr>
+            <td>NIM</td>
+            <td>:<input type="number" name="nim"></td>
+        </tr>
+        <tr>
+            <td>pilih gambar</td>
+            <td>: <input type="file" name="fileup"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="kirim" name="submit"></td>
+        </tr>
+    </form>
+</table>
---
+++ output3tampil.php
@@ -0,0 +1,13 @@
+<?php
+    $nama = $_POST["nama"];
+    $tgl = $_POST["tgl"];
+    $nim = $_POST["nim"];
+
+    echo "nama : ".$nama."<br> tanggal lahir : ".$tgl."<br> NIM : ".$nim.
+    "<br> foto diri : <img src='./uploads/gambaraku.jpg' alt=''>";
+    $nama = $_FILES['fileup']['name'];
+		$lokasi = $_FILES['fileup']['tmp_name'];
+		$namabaru = "gambaraku.jpg";
+		move_uploaded_file($lokasi, "uploads/".$namabaru);
+?>
+
